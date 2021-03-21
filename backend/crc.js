0a9f08b5d16901bda3751595e3237252e563be6c
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-03-21 13:42:43 +0200

    refactor crc

--- backend/crc.js
+++ backend/crc.js
@@ -1,55 +1,75 @@
 'use strict';
 
-const createCrcTable = function(width, castMask, polynom) {
-  const msbMask = 0x01 << (width - 1);
-  const crcTable = new Array(256);
 
-  for (let i = 0; i < 256; i++) {
-    let byte = (i << (width - 8));
-    for (let bit = 0; bit < 8; bit++) {
-      if (byte & msbMask) {
-        byte <<= 1;
-        byte ^= polynom;
-      } else {
-        byte <<= 1;
+class CRC {
+  constructor(width, polynom, initialVal, finalXorVal) {
+
+    // width: [castMask, polynom, initialVal, finalXorVal]
+    const crcParamsByWidth = {
+      '8': [0xFF, 0x07, 0x00, 0x00],
+      '16': [0xFFFF, 0x8005, 0x0000, 0x0000],
+      '32': [0xFFFFFFFF, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF]
+    };
+
+    this.width = width;
+    const paramsByWidth = crcParamsByWidth[width];
+    if (paramsByWidth) {
+      [this.castMask, this.polynom, this.initialVal, this.finalXorVal] = paramsByWidth;
+    } else {
+      throw 'Invalid CRC width';
+    }
+
+    if (arguments.length == 4) {
+      this.polynom = arguments[1];
+      this.initialVal = arguments[2];
+      this.finalXorVal = arguments[3];
+    } else if (arguments.length != 1) {
+      throw "Invalid arguments";
+    }
+
+    if (!Number.isInteger(this.polynom) ||
+      !Number.isInteger(this.initialVal) ||
+      !Number.isInteger(this.finalXorVal))
+      throw "Invalid arguments";
+
+    this.table = this.createCrcTable();
+
+  }
+
+  createCrcTable() {
+    const msbMask = 0x01 << (this.width - 1);
+    const table = new Array(256);
+
+    for (let i = 0; i < 256; i++) {
+      let byte = (i << (this.width - 8));
+      for (let bit = 0; bit < 8; bit++) {
+        if (byte & msbMask) {
+          byte <<= 1;
+          byte ^= this.polynom;
+        } else {
+          byte <<= 1;
+        }
       }
+      table[i] = (byte & this.castMask);
     }
-    crcTable[i] = (byte & castMask);
-  }
-  return crcTable;
-};
-
-const crcParamsByWidth = {
-  '8': [0xFF, 0x07, 0x00, 0x00],
-  '16': [0xFFFF, 0x8005, 0x0000, 0x0000],
-  '32': [0xFFFFFFFF, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF]
-};
-
-const partial = (fn, ...args) => (...rest) => fn(...args, ...rest);
-
-const getCrcByConfig = (crcConfig, width) => {
-  let castMask, polynom, initialVal, finalXorVal;
-  const paramsByWidth = crcConfig[width];
-  if (paramsByWidth) {
-    [ castMask, polynom, initialVal, finalXorVal ] = paramsByWidth;
-  } else {
-    throw 'Invalid CRC width';
+    return table;
   }
 
-  const table = createCrcTable(width, castMask, polynom);
 
-  return (bytes) => {
-    let crc = initialVal;
-    for (const b of bytes) {
+  calcCrc(bytes) {
+    if (!Array.isArray(bytes) || bytes.length == 0)
+      throw "It's not a array or empty array"
+    let result = this.initialVal;
+    for (let b of bytes) {
       const byte = parseInt(b, 16);
-      crc ^= (byte << (width - 8));
-      const pos = (crc >> (width - 8));
-      crc = ((crc << 8) ^ table[pos]) & castMask;
+      result ^= (byte << (this.width - 8));
+      const pos = (result >> (this.width - 8));
+      result = ((result << 8) ^ this.table[pos]) & this.castMask;
     }
-    return (crc ^ finalXorVal);
-  };
-};
+    return (result ^ this.finalXorVal);
+  }
 
-const getCrc = partial(getCrcByConfig, crcParamsByWidth);
+}
 
-module.exports = getCrc;
+module.exports = CRC;
+
