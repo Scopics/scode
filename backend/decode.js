6949749ff3ce76778eed5279ccb7ebcd159609cc
MaksGovor <maksgovruha@gmail.com>
2021-03-21 12:30:01 +0200

    Add helper for decoder func

--- backend/decode.js
+++ backend/decode.js
@@ -45,3 +45,14 @@ function removeGuides(rays) {
   guidesIndexes.reverse().forEach(index => raysCopy.splice(index, 1));
   return raysCopy;
 }
+
+const getChunksOfString = (str, size) => {
+  const numChunks = Math.ceil(str.length / size);
+  const chunks = new Array(numChunks);
+
+  for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+    chunks[i] = str.substr(o, size);
+  }
+  return chunks;
+};
+
