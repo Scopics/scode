521d446d06a5aff0b8ddd254e9029c269688f10a
Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>
2021-03-21 13:57:18 +0200

    Update stabilize according to suggestions
    Co-authored-by: YAGoOaR <55108635+YAGoOaR@users.noreply.github.com>

--- backend/src/decode.js
+++ backend/src/decode.js
@@ -6,32 +6,26 @@ function stabilize(rays) {
   if (rays.length % 4 !== 0)
     throw new Error('Something is wrong...');
   const len = rays.length;
-  const step = len / 4;
+  const sides = 4;
+  const step = len / sides;
 
   let startFound = false;
   let iterator = 0;
-  const neededLen = {
-    top: 15,
-    right: 15,
-    bottom: 0,
-    left: 15
-  };
+  const neededLen = [15, 15, 0, 15];
+
   while (!startFound && iterator < len) {
-    const topInd = iterator % len;
-    const top = rays[topInd];
-    const rightInd = (iterator + step) % len;
-    const right = rays[rightInd];
-    const bottomInd = (iterator + step * 2) % len;
-    const bottom = rays[bottomInd];
-    const leftInd = (iterator + step * 3) % len;
-    const left = rays[leftInd];
-    if (neededLen.top === top &&
-      neededLen.right === right &&
-      neededLen.bottom === bottom &&
-      neededLen.left === left
-    ) {
+    const correct = true;
+
+    for(let i = 0; i < sides; i++){
+      const ind = (iterator + (step * i)) % len;
+      if(neededLen[i] !== rays[ind]){
+        correct = false
+      }
+    }
+    if(correct){
       startFound = true;
-    } else {
+    }
+    else {
       iterator++;
     }
   }
