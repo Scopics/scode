469a3f5d1e203666ff4351cc92466c5336c0ca3d
MaksGovor <maksgovruha@gmail.com>
2021-03-22 13:02:37 +0200

    Add tests for codeASCII

--- backend/src/test/crc.test.js
+++ backend/src/test/crc.test.js
@@ -108,7 +108,3 @@ describe('Testing CRC', () => {
   });
 
 });
-
-
-
-
--- backend/src/test/utils.test.js
+++ backend/src/test/utils.test.js
@@ -1,41 +1,83 @@
 'use strict';
 
-const { codeItemASCII } = require('../utils');
+const { codeItemASCII, codeASCII } = require('../utils');
 
 describe('Testing codeItemASCII', () => {
-    test('Passes when pass valid parameters', () => {
-      const char = 'a';
-      const result = codeItemASCII(char);
-      const expectedResult = '61';
-      expect(result).toEqual(expectedResult);
-    });
-  
-    test('Fails when pass a number', () => {
-      const char = 1;
-      expect(() => {
-        codeItemASCII(char);
-      }).toThrowError('Argument is not a char');
-    });
-  
-    test('Fails when pass an array', () => {
-      const char = ['a', 'b', 'c'];
-      expect(() => {
-        codeItemASCII(char);
-      }).toThrowError('Argument is not a char');
-    });
-  
-    test('Fails when pass not a char but with charCode key', () => {
-      const char = { charCodeAt: () => 5 };
-      expect(() => {
-        codeItemASCII(char);
-      }).toThrowError('Argument is not a char');
-    });
-  
-    test('Fails when pass empty string', () => {
-      const char = '';
-      expect(() => {
-        codeItemASCII(char);
-      }).toThrowError('Argument is not a char');
-    });
+  test('Passes when pass valid parameters', () => {
+    const char = 'a';
+    const result = codeItemASCII(char);
+    const expectedResult = '61';
+    expect(result).toEqual(expectedResult);
   });
+
+  test('Fails when pass a number', () => {
+    const char = 1;
+    expect(() => {
+      codeItemASCII(char);
+    }).toThrowError('Argument is not a char');
+  });
+
+  test('Fails when pass an array', () => {
+    const char = ['a', 'b', 'c'];
+    expect(() => {
+      codeItemASCII(char);
+    }).toThrowError('Argument is not a char');
+  });
+
+  test('Fails when pass not a char but with charCode key', () => {
+    const char = { charCodeAt: () => 5 };
+    expect(() => {
+      codeItemASCII(char);
+    }).toThrowError('Argument is not a char');
+  });
+
+  test('Fails when pass empty string', () => {
+    const char = '';
+    expect(() => {
+      codeItemASCII(char);
+    }).toThrowError('Argument is not a char');
+  });
+});
   
+
+describe('Testing codeASCII', () => {
+  test('Passes when pass valid parameters', () => {
+    const text = '1<T';
+    const result = codeASCII(text);
+    
+    const expectedResult = ['31', '3c', '54'];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Passes when pass emty string', () => {
+    const text = '';
+    const result = codeASCII(text);
+    
+    const expectedResult = [];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Passes when pass number', () => {
+    const text = 9101;
+    const result = codeASCII(text);
+    
+    const expectedResult = ['39', '31', '30', '31'];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Passes when pass array of strings', () => {
+    const text = ['1', '<', 'T'];
+    const result = codeASCII(text);
+    
+    const expectedResult = ['31', '3c', '54'];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Passes when pass array of nubmers', () => {
+    const text = [ 9, 1, 0, 1];
+    const result = codeASCII(text);
+    
+    const expectedResult = ['39', '31', '30', '31'];
+    expect(result).toEqual(expectedResult);
+  });
+});
