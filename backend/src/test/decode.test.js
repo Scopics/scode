81dab79d90eabf9b960141b7b084b58872dca84b
YAGoOaR <yegorgribenko68@gmail.com>
2021-03-22 12:17:39 +0200

    Add removeGuides tests

--- backend/src/test/decode.test.js
+++ backend/src/test/decode.test.js
@@ -121,3 +121,55 @@ describe('Testing stabilize', () => {
     expect(() => stabilize(rays)).toThrowError(expected);
   });
 });
+
+describe('Testing removeGuides', () => {
+  test('Passes when pass valid parameters', () => {
+    const rays = [1, 5, 10, 15, 2, 5, 14, 12, 1, 9, 5, 11, 10, 7, 3, 5];
+    const result = removeGuides(rays);
+    const expectedResult = [5, 10, 15, 5, 14, 12, 9, 5, 11, 7, 3, 5];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('Fails when pass array length that is not divisable by 4', () => {
+    const rays = [1, 5, 10, 15, 2, 5, 14, 12, 1, 9, 5, 11, 10, 7, 3, 5, 10];
+    expect(() => {
+      removeGuides(rays);
+    }).toThrowError('Invalid array length');
+  });
+
+  test('Fails when pass empty array', () => {
+    const rays = [];
+    expect(() => {
+      removeGuides(rays);
+    }).toThrowError('Array is empty or it is not an array');
+  });
+
+  test('Fails when pass not an array', () => {
+    const rays = { key1: value1 };
+    expect(() => {
+      removeGuides(rays);
+    }).toThrowError('Array is empty or it is not an array');
+  });
+
+  test('Fails when pass a string', () => {
+    const rays = '01230123012301230123';
+    expect(() => {
+      removeGuides(rays);
+    }).toThrowError('Array is empty or it is not an array');
+  });
+
+  test('Fails when pass not an array but with symbol.iterator', () => {
+    const rays = {
+      name: 'Trojan horse',
+      length: 100,
+      [Symbol.iterator]: function*(){
+        yield 1;
+        yield 2;
+        yield 3;
+      }
+    };
+    expect(() => {
+      removeGuides(rays);
+    }).toThrowError('Array is empty or it is not an array');
+  });
+});
