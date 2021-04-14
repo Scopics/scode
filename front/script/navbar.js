6ec4e65856fa14af0e81516e561109d0c09d49dc
dmytro <tedi4t@gmail.com>
2021-04-14 21:42:44 +0300

    add hiding pop-up window

--- front/script/navbar.js
+++ front/script/navbar.js
@@ -4,3 +4,7 @@ $('header a').click(function(e) {
   $('.pop-up').css('transform', 'translateX(110%)');
   $(`section${sectionName}.pop-up`).css('transform', 'translateX(0)');
 })
+
+$('body').on('click', ':not(header, header *, .pop-up, .pop-up *)', function(e) {
+  $('.pop-up').css('transform', 'translateX(110%)');
+})
