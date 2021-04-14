2061b1863632b74cfe7f1b45e18608fcca1bb4ed
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-04-14 21:56:44 +0300

    fix app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DecodeModule } from './decode/decode.module';
-import { UtilsModule } from './utils/utils.module';
 import { EncodeController } from './encode/encode.controller';
 import { DecodeController } from './decode/decode.controller';
-import { EncodeModule } from './encode/encode.module';
 
 @Module({
-  imports: [DecodeModule, UtilsModule],
+  imports: [],
   controllers: [AppController, EncodeController, DecodeController],
   providers: [AppService],
 })
