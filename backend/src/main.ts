1e20f2745f898fe36a55064cdc12eb1c1a63374a
dmytro <tedi4t@gmail.com>
2021-04-16 18:28:53 +0300

    enabled cors for develop version

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,10 @@ import { UtilsModule } from './utils/utils.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
