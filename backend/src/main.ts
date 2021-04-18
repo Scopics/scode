610332d88cc7b9e37087ad1d838998bc2b1908af
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-04-18 21:22:38 +0300

    add rendering for html

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { UtilsModule } from './utils/utils.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: '*',
     credentials: true,
   });
+  app.useStaticAssets(join(__dirname, '../front'));
   await app.listen(8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
