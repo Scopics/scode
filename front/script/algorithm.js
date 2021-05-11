b7184b5f59c8ad6a30fcda4d0ff34c2e59095c11
YAGoOaR <yegorgribenko68@gmail.com>
2021-05-11 18:52:39 +0300

    Add rays end recognition (Needs refactor!)

--- front/script/algorithm.js
+++ front/script/algorithm.js
@@ -1,3 +1,5 @@
+const DIAGONAL = Math.sqrt(2);
+
 const CIRCLE_START = 0;
 const CIRCLE_END = 2 * Math.PI;
 const PIXEL_DATA_LENGTH = 4;
@@ -105,13 +107,205 @@ function getMatrix(img){
         for(let j = 0; j < row; j ++){
             const pixel = img.data[4 * i * row + 4 * j] > 128 ? 0 : 1;
             matrix[i].push(pixel);
-            ctx2.beginPath();
-            ctx2.fillStyle = pixel ? 'red' : 'blue';
-            ctx2.fillRect(j, i, 0.8, 0.8);
-            ctx2.fill();
         }
     }
     return matrix;
+}
+
+function exploreLine(matrix, startPos = new Vector2()){
+    const h = matrix.length;
+    const w = matrix[0].length;
+    const openedNodes = [];
+    const closedNodes = [];
+    if(!matrix[startPos.x][startPos.y]){
+        console.log('fail');
+        return null;
+    }
+    const startNode = { pos: startPos, g: 0, parent: null };
+    openedNodes.push(startNode);
+    let curr = startNode;
+    let counter = 0;
+    let thebest = curr;
+    while(openedNodes.length > 0 && counter < 4000){
+        counter++;
+        if(counter === 4000) throw(new Error("too many iterations"));
+        let bestNode = openedNodes[0];
+        for (let i = 1; i < openedNodes.length; i++){
+            let nextNode = openedNodes[i];
+            if (nextNode.g > bestNode.g){
+               bestNode = nextNode;
+            }
+        }
+        if(bestNode.g > thebest.g){
+            thebest = bestNode;
+        }
+
+        curr = bestNode;
+        openedNodes.splice(openedNodes.indexOf(curr), 1);
+        closedNodes.push(curr);
+        for(let i = -1; i <= 1; i++){
+            for(let j = -1; j <= 1; j++){
+                if((i !== 0 || j !== 0)){
+                    const x = curr.pos.x + i;
+                    const y = curr.pos.y + j;
+                    if(x < 0 || y < 0 || x >= w || y >= h) continue;
+                    if(!matrix[x][y]) continue;
+                    let b = false;
+                    for (const node of closedNodes){
+                      if (node.pos.x === curr.pos.x + i && node.pos.y === curr.pos.y + j) {
+                        b = true;
+                        break;
+                      }
+                    }
+                    const newG = curr.g + (Math.abs(i) + Math.abs(j) === 1 ? 1 : DIAGONAL);
+                    
+                    if (!b){
+                        const neighbour = { pos: curr.pos.add(new Vector2(i, j)), g: newG , parent: curr };
+
+                        let c = false;
+                        for (const node of openedNodes){
+                            if (Vector2.equals(node.pos, neighbour.pos)){
+                                c = true;
+                            }
+                        }   
+                        if(!c || newG < neighbour.g){
+                            neighbour.g = newG;
+                            if(!c) openedNodes.push(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+        if (openedNodes.length === 0){
+            const res = [];
+            let parent = thebest;
+            while (parent.parent) {
+                parent = parent.parent;
+                if(!parent.parent) res.push(parent.pos);
+            }
+            res.push(thebest.pos);
+            // for (const node of closedNodes){
+            //     ctx2.beginPath();
+            //     ctx2.fillStyle = 'red';
+            //     ctx2.fillRect(node.pos.y, node.pos.x, 0.8, 0.8);
+            //     ctx2.fill();
+            // }
+            return res;
+        }
+    }
+}
+
+function getLine(matrix, startPos){
+    const line = exploreLine(matrix, startPos);
+    const endPos = line[1];
+    const line2 = exploreLine(matrix, endPos);
+
+    const a = line2[0];
+    const b = line2[1];
+
+    ctx2.strokeStyle = 'green';
+    ctx2.lineWidth = 4;
+
+    ctx2.beginPath();
+    ctx2.moveTo(a.y, a.x);
+    ctx2.lineTo(b.y, b.x);
+    ctx2.stroke();
+
+    ctx2.strokeStyle = 'red';
+    ctx2.lineWidth = 2;
+
+    ctx2.beginPath();
+    ctx2.arc(a.y, a.x, 3, CIRCLE_START, CIRCLE_END);
+    ctx2.stroke();
+
+    ctx2.beginPath();
+    ctx2.arc(b.y, b.x, 3, CIRCLE_START, CIRCLE_END);
+    ctx2.stroke();
+
+    return line2;
+}
+
+function findlines(matrix){
+    const lines = [];
+    const h = matrix.length;
+    const w = matrix[0].length;
+    let cntr = 0;
+    for(let i = 0; i < h; i++){
+        for(let j = 0; j < w; j++){
+            if(!matrix[i][j]) continue;
+            // if(cntr > 3342) {
+            //     const a = lines[lines.length - 1][0];
+            //     ctx2.strokeStyle = 'green';
+            //     ctx2.lineWidth = 4;
+
+            //     ctx2.beginPath();
+            //     ctx2.arc(a.y, a.x, 5, CIRCLE_START, CIRCLE_END);
+            //     ctx2.stroke();
+
+            //     const b = lines[lines.length - 1][1];
+            //     ctx2.strokeStyle = 'blue';
+            //     ctx2.lineWidth = 4;
+
+            //     ctx2.beginPath();
+            //     ctx2.arc(b.y, b.x, 5, CIRCLE_START, CIRCLE_END);
+            //     ctx2.stroke();
+
+            //     console.log(lines.length);
+            //     return;
+            // };
+            cntr++;
+            let bool = false;
+            for(const line of lines){
+                const V2 = line[1].subtract(line[0]);
+                const V = V2.rotate(Math.PI / 2);
+                // const a = line[0];
+                // const b = line[0].add(V);
+    
+                // ctx2.strokeStyle = 'red';
+                // ctx2.lineWidth = 4;
+    
+                // ctx2.beginPath();
+                // ctx2.moveTo(a.y, a.x);
+                // ctx2.lineTo(b.y, b.x);
+                // ctx2.stroke();
+                // console.log(V);
+
+                const A = V.x;
+                const B = V.y;
+                const C = - A * line[0].x - B * line[0].y; 
+                const dist = Math.abs(A * i + B * j + C) / Math.sqrt(A * A + B * B);
+
+                const mid = line[0].add(line[1]).divide(2);
+                const A2 = V2.x;
+                const B2 = V2.y;
+                const C2 = - A2 * mid.x - B2 * mid.y; 
+                const dist2 = Math.abs(A2 * i + B2 * j + C2) / Math.sqrt(A2 * A2 + B2 * B2);
+
+                // const a = mid;
+                // const b = mid.add(V);
+    
+                // ctx2.strokeStyle = 'red';
+                // ctx2.lineWidth = 4;
+    
+                // ctx2.beginPath();
+                // ctx2.moveTo(a.y, a.x);
+                // ctx2.lineTo(b.y, b.x);
+                // ctx2.stroke();
+                const border = 3;
+                const len = V2.length() / 2;
+                if(dist < 3 && dist2 <= len + 3){
+                    bool = true;
+                    break;
+                }
+            }
+
+            if(bool) continue;
+
+            const line = getLine(matrix, new Vector2(i, j));
+            lines.push(line);
+        }
+    }
+    console.log(lines.length);
 }
 
 function onLoad(){
@@ -135,6 +329,8 @@ function onLoad(){
     } else {
         console.log('Image is not scode');        
     }
+
+    findlines(matrix);
 
     ctx2.strokeStyle = 'red';
 
--- front/script/utils.js
+++ front/script/utils.js
@@ -5,3 +5,51 @@ function drawline(ctx, a, b){
   ctx.lineTo(b.x, b.y);
   ctx.stroke();
 }
+
+class Vector2 {
+  constructor(_x = 0, _y = 0) {
+    this.x = _x;
+    this.y = _y;
+  }
+
+  static middle = new Vector2(400, 400);
+
+  static Zero = new Vector2(0, 0);
+
+  static getAngle = (vector) => Math.acos(vector.x / vector.length()) * Math.sign(vector.y);
+
+  static makeFromAngle = (magnitude, _angle) => {
+    return new Vector2(Math.cos(_angle) * magnitude, Math.sin(_angle) * magnitude);
+  }
+
+  static equals (vector1, vector2){
+    return vector1.x === vector2.x && vector1.y === vector2.y;
+  }
+
+  length = () => Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  normalized = () =>{
+    return new Vector2(this.x / this.length(), this.y / this.length())
+  }
+  add = (_vector) => {
+    return new Vector2(this.x + _vector.x, this.y + _vector.y)
+  }
+  subtract = (_vector) => {
+    return new Vector2(this.x - _vector.x, this.y - _vector.y)
+  }
+  reversed = () => {
+    return new Vector2(-this.x, -this.y)
+  }
+
+  rotate = (_angle) => {
+      return Vector2.makeFromAngle(this.length(), Vector2.getAngle(this) + _angle);
+  }
+
+  multiply = (n) => new Vector2(this.x * n, this.y * n);
+
+  divide = (n) => new Vector2(this.x / n, this.y / n);
+
+  ToString = () => {
+    return '{ ' + Math.round(this.x) + ', ' + Math.round(this.y) + ' }';
+  }
+
+}
